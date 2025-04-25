c30dc6741cfd2271d227f547078b4930e3d6a086
Pierre GIRAUDEAU <pgiraude@made-f0Br3s9.clusters.42paris.fr>
2025-04-25 12:52:50 +0200

    p3

--- p2/scripts/install-dependancies.sh
+++ p2/scripts/install-dependancies.sh
@@ -16,7 +16,7 @@ until kubectl get nodes; do
 done
 
 # Install Helm
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg 
 sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
